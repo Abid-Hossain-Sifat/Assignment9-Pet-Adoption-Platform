0465222f2e3d789c6d5b361e37ca6ed6a36ebdbf
Abid-Hossain-Sifat <itzabid006@gmail.com>
2026-05-18 03:05:10 +0600

    Added subscribe section

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import Process from "@/Components/Process";
 import WhyAdopt from "@/Components/WhyAdopt";
 import Review from "@/Components/Review";
 import PetCare from "@/Components/PetCare";
+import Connect from "@/Components/Connect";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <Process />
       <Review />
       <PetCare />
+      <Connect></Connect>
     </div>
   );
 }
