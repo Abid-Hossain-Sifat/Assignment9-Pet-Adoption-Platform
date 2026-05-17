a58746050a3a42dd8194f2dadf4e81d31090e8a7
Abid-Hossain-Sifat <itzabid006@gmail.com>
2026-05-18 01:47:02 +0600

    added why adopt section

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Hero from "@/Components/Hero";
+import WhyAdopt from "@/Components/WhyAdopt";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <Hero></Hero>
+      <WhyAdopt />
     </div>
   );
 }
